4760a411ca8ec76d59dc18e8900ec8542ce355f3
HyooRin <khl4459@naver.com>
2023-04-04 18:00:22 +0900

    Hboard 쿼리 추가

--- DB 연결 hboard 테이블 생성 및 쿼리.sql
+++ DB 연결 hboard 테이블 생성 및 쿼리.sql
@@ -3,36 +3,84 @@
 
 -- USER, board, reply
 CREATE TABLE user(
-	id int PRIMARY KEY AUTO_INCREMENT,     
-    email VARCHAR(20) UNIQUE NOT NULL,
+	id int PRIMARY KEY AUTO_INCREMENT,   
+    userName VARCHAR(10) NOT NULL,
+    email VARCHAR(20) NOT NULL,
     password VARCHAR(10) NOT NULL,
     nickName VARCHAR(10) UNIQUE NOT NULL
 );
 
+DESC user;
+DESC board;
+
 CREATE TABLE board (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
-    content TEXT,
-    nickName VARCHAR(10),
+    content TEXT,    
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (nickName) REFERENCES user (nickName)
+    userId int,
+    FOREIGN KEY (userId) REFERENCES user (id)
 );
 
 -- 아직 생성안함 
-CREATE TABLE reply (
+ -- CREATE TABLE reply (
     id INT PRIMARY KEY AUTO_INCREMENT,
     replyContent VARCHAR(200),
     boardId INT NOT NULL,
     FOREIGN KEY (boardId) REFERENCES board (id)
-);
+); 
 
-DROP TABLE user;
-DROP TABLE board;
+--  DROP TABLE user;
+-- DROP TABLE board;
+-- DROP TABLE reply ;
 
 SELECT * FROM user;
 SELECT * FROM board;
 
+INSERT INTO board(title, content, userId )
+VALUES ('해보자', '가자', 1 );
 
-INSERT INTO user(userName, email)
-VALUES('효린스','khl4459@naver.com'),
-	  ('주이스','joi@naver.com');
+SELECT *
+FROM user
+WHERE userName = 'khl4459' AND email = 'naver.com' AND password = '1234'; 
+
+      
+UPDATE user SET nickName = '될까요' WHERE id = '3';
+	 
+      
+SELECT title, content, nickName, date
+FROM board
+WHERE title = '자바';
+
+SELECT *
+FROM user 
+WHERE userName = 'khl4459' AND email = 'gmail.com' AND password = '1234';
+
+DELETE FROM user WHERE id = 5;
+
+UPDATE user SET userName = 'khl4459', email = 'gmail.com', 
+password = '1234', nickName = '효린스'
+WHERE id = 1;  
+
+SELECT * FROM user;
+SELECT * FROM board;
+
+DELETE FROM user WHERE id = 4;
+
+
+SELECT password FROM user WHERE userName = 'khl4459' AND email = 'gmail.com';
+
+-- dto 
+desc board;
+select * from user where id = 1;
+
+SELECT * FROM board;
+SELECT * FROM user;
+
+-- 
+select b.*, nickName 
+from board as b 
+inner join user as u
+on b.userId = u.id;
+
+-- where b.userId = 1; 

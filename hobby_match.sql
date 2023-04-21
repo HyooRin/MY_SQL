3e5312f7fc3e6e6bd94f7b52ad77b73d0fa58b5d
HyooRin <khl4459@naver.com>
2023-04-21 17:45:45 +0900

    Hobby Project DB

---
+++ hobby_match.sql
@@ -0,0 +1,83 @@
+create database if not exists HOBBIES_SHARE;
+use HOBBIES_SHARE;
+drop table if exists Q_A;
+drop table if exists MEMBER_MATCH;
+drop table if exists MEMBER_HOBBIES;
+drop table if exists HOBBIES;
+drop table if exists comment_tb;
+drop table if exists board_tb;
+drop table if exists USER_TB;
+
+-- 유저
+create table if not exists USER_TB(
+	id bigint primary key auto_increment,
+    email varchar(100) unique not null,
+    password varchar(100) not null,
+    username varchar(100) not null,
+    birth date not null,
+    phone varchar(50),
+    user_role varchar(100) not null default 'user',
+    created_at timestamp default now()
+);
+
+-- 게시판
+create table if not exists BOARD_TB(
+	id bigint primary key auto_increment,
+    title varchar(100) not null,
+    content TEXT not null,
+    user_id bigint not null,
+    hobby_id bigint not null,
+    created_at timestamp default now(),
+    foreign key(user_id) references USER_TB(id) on update cascade on delete cascade,
+    foreign key(hobby_id) references HOBBIES(id) on update cascade on delete cascade
+);
+
+-- 댓글
+create table if not exists COMMENT_TB(
+	id bigint primary key auto_increment,
+    content varchar(1024) not null,
+	user_id bigint,
+    board_id bigint not null,
+    created_at timestamp default now(),
+    foreign key(user_id) references USER_TB(id) on update cascade on delete cascade,
+    foreign key(board_id) references BOARD_TB(id) on update cascade on delete cascade
+);
+
+-- 취미목록
+create table if not exists HOBBIES(
+	id bigint primary key auto_increment,
+	hobby varchar(1024) not null
+);
+
+-- 유저들의 취미(유저는 많은 취미를 가질 수 있음)
+create table if not exists MEMBER_HOBBIES(
+	id bigint primary key auto_increment,
+    hobby bigint not null,
+    user_id bigint not null,
+    created_at timestamp default now(),
+    foreign key(hobby) references HOBBIES(id) on update cascade on delete cascade,
+    foreign key(user_id) references USER_TB(id) on update cascade on delete cascade
+);
+
+-- 유저 만남 기록
+create table if not exists MEMBER_MATCH(
+	main_user bigint not null,
+    sub_user bigint not null,
+    created_at timestamp default now(),
+    foreign key(main_user) references USER_TB(id) on update cascade on delete cascade,
+    foreign key(sub_user) references USER_TB(id) on update cascade on delete cascade
+);
+
+create table if not exists Q_A(
+	id bigint primary key auto_increment,
+    user_id bigint not null,
+    content TEXT not null,
+    foreign key(user_id) references USER_TB(id) on update cascade on delete cascade
+);
+
+
+-- 
+
+
+
+
